3323fb651f51067ffa8d9a25cec66adf3c60e1b6
debbyji <jasmine820921@gmail.com>
2018-06-04 22:18:03 +0800

    add todolist.vue

---
+++ src/pages/todolist.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="wrapper">
+        <h1>hello</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'todolist'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/pages/todolist/_pages.scss";
+
+h1 {
+  color: $main_blue;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import home from '@/pages/home.vue'
+import todolist from '@/pages/todolist.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/todolist',
+      name: 'todolist',
+      component: todolist
+    }
+  ]
+})
