07997c201bfd0091a72245d8f5468eac71b90fb1
debbyji <jasmine820921@gmail.com>
2018-06-15 22:46:48 +0800

    新增filter專案

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import home from '@/pages/home.vue';
 import todolist from '@/pages/todolist/index.vue';
+import filter from '@/pages/filter/index.vue';
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/todolist',
       name: 'todolist',
       component: todolist
+    },
+    {
+      path: '/filter',
+      name: 'filter',
+      component: filter
     }
   ]
 });
